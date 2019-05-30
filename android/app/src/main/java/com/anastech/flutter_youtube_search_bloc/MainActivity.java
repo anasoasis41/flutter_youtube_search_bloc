75841997a853b8ca353885fc9bdeab71ffa8a336
Anas Riahi <anas.riahi@smile-maroc.com>
2019-05-30 14:13:58 +0000

    Initial commit

---
+++ android/app/src/main/java/com/anastech/flutter_youtube_search_bloc/MainActivity.java
@@ -0,0 +1,13 @@
+package com.anastech.flutter_youtube_search_bloc;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
